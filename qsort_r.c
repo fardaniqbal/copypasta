9844427ca2d5ae645e63c5f5b3c8f5468eba5978
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2021-11-10 20:01:44 -0500

    TODO: implement qsort_r.

--- qsort_r.c
+++ qsort_r.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 /*
+TODO: implement this
 void qsort_r(void *base, size_t nmemb, size_t size,
              int (*compar)(const void *, const void *, void *),
              void *arg);
