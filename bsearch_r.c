979395ffbc22d3a2500b7f6258b2c59dcc981d8b
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2021-11-10 20:48:49 -0500

    Start adding tests.

--- bsearch_r.c
+++ bsearch_r.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 
-/* - test ------------------------------------------------------------------ */
+/*
+void *bsearch_r(const void *key, const void *base, size_t nmemb,
+                size_t size,
+                int (*compar)(const void *, const void *, void *),
+                void *arg);
+*/
+
+/* - test -------------------------------------------------------------- */
 
 int
 main(void)
 {
+  int sorted_arr[] = {
+    1, 3, 5, 6, 7, 10, 11, 12, 13, 14, 26, 29, 20, 22, 23, 24, 25, 26, 27,
+    28, 29, 30, 31, 32, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 49
+  };
   /* TODO */
   return 0;
 }
