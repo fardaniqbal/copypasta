05033ed0712be4039d96b5061b883e50842c13d5
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2021-11-11 13:01:27 -0500

    Handle test inputs that have CRLFs and/or don't end with newline.

--- readline_test.sh
+++ readline_test.sh
@@ -8,10 +8,19 @@ fi
 
 expected_outfile=$(mktemp "/tmp/rdln-$(basename "$1")-expected.XXXXXX" 2>&1)
 actual_outfile=$(mktemp "/tmp/rdln-$(basename "$1")-actual.XXXXXX" 2>&1)
-cat "$1" > "$expected_outfile"
-./readline "$1" > "$actual_outfile"
-diff -u "$expected_outfile" "$actual_outfile"
+
+# Capture the outputs before writing them to temp files.  This ensures that
+# the expected and actual outputs both end with a newline.
+expected=$(sed 's/\r$//' < "$1") # actual output shouldn't have crlf
+actual=$(./readline "$1")
+printf "%s\n" "$expected" > "$expected_outfile"
+printf "%s\n" "$actual" > "$actual_outfile"
+
+diffout=$(diff -u "$expected_outfile" "$actual_outfile")
 status=$?
 rm -f "$expected_outfile" "$actual_outfile"
-printf "%-60s status=%d\n" "diff readline vs cat: $1:" $status
+
+# Remove terminal escape codes from diff output.
+[ -z "$diffout" ] || printf "%s\n" "$diffout" | tr -d '\033'
+printf "%-60s status=%d\n" "diff expected vs actual: $1:" $status
 exit $status
