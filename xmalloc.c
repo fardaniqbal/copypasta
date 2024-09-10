70d4120b2f9c892c77cc045b34fb7d7d35e00bcb
Fardan Iqbal <fardaniqbal+dev@gmail.com>
2024-09-10 15:35:42 -0400

    xmalloc.c: print wide sizes if possible on alloc failure

--- xmalloc.c
+++ xmalloc.c
@@ -1,7 +1,6 @@
 /* Public domain implementation of xmalloc/xrealloc/xcalloc.  These macros
    behave like their corresponding standard C functions, except they crash on
    allocation failure with a message indicating file and line of the crash. */
-#include <stdio.h>
 #include <stdlib.h>
 
 #define xmalloc(sz) \
@@ -11,29 +10,53 @@
 #define xcalloc(n, sz) \
   xcalloc_(n, sz, __FILE__, __LINE__, "xcalloc(" #n ", " #sz ")")
 
-static void *
+void *xrealloc_(void *, size_t, const char *file, int line, const char *expr);
+void *xcalloc_(size_t, size_t, const char *file, int line, const char *expr);
+
+/* - can put the above in a header and the below in a C/C++ file ----------- */
+
+#include <limits.h> /* for LLONG_MAX */
+#include <stdio.h>
+
+#ifdef LLONG_MAX /* "%llu" seems to be more portable than "%zu" */
+# if defined _MSC_VER || (defined __MINGW32__ && !__USE_MINGW_ANSI_STDIO)
+#   define XMALLOC_PRINTF_SIZE "I64u"
+# else
+#   define XMALLOC_PRINTF_SIZE "llu"
+# endif
+# ifdef __GNUC__
+__extension__
+# endif
+typedef unsigned long long xmalloc_printf_size_t;
+#else
+# define XMALLOC_PRINTF_SIZE "lu"
+typedef unsigned long xmalloc_printf_size_t;
+#endif
+
+void *
 xrealloc_(void *ptr, size_t sz, const char *file, int line, const char *expr)
 {
   if ((ptr = realloc(ptr, sz)) != NULL || sz == 0)
     return ptr;
   fprintf(stderr, "%s:%d: %s failed.\n", file, line, expr);
-  fprintf(stderr, "%s:%d: not enough memory to allocate %lu bytes.\n",
-          file, line, (unsigned long) sz);
+  fprintf(stderr, "%s:%d: not enough memory to allocate "
+          "%" XMALLOC_PRINTF_SIZE " bytes.\n", file, line,
+          (xmalloc_printf_size_t) sz);
   exit(1);
 }
 
-static void *
+void *
 xcalloc_(size_t n, size_t sz, const char *file, int line, const char *expr)
 {
   void *ptr = calloc(n, sz);
   if (ptr != NULL || n == 0 || sz == 0)
     return ptr;
   fprintf(stderr, "%s:%d: %s failed.\n", file, line, expr);
-  fprintf(stderr, "%s:%d: not enough memory to allocate %lu x %lu bytes.\n",
-          file, line, (unsigned long) n, (unsigned long) sz);
+  fprintf(stderr, "%s:%d: not enough memory to allocate "
+          "%" XMALLOC_PRINTF_SIZE " x %" XMALLOC_PRINTF_SIZE " bytes.\n",
+          file, line, (xmalloc_printf_size_t) n, (xmalloc_printf_size_t) sz);
   exit(1);
 }
-typedef int xmalloc_unused_[!!sizeof &xrealloc_ + !!sizeof &xcalloc_];
 
 /* - test ------------------------------------------------------------------ */
 
